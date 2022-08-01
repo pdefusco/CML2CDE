1e2f95278f2e872438bad5c60cb9857b3d7d6e32
Paul de Fusco <pauldefusco@cloudera.com>
2022-08-01 22:48:46 +0000

    finished the four basic operators

--- airflow_dags/oozie2airflow.py
+++ airflow_dags/oozie2airflow.py
@@ -7,6 +7,12 @@ from datetime import timezone
     
 from airflow import DAG
     
+from airflow.operators.email import EmailOperator
+    
+from airflow.operators.python_operator import PythonOperator
+    
+from cloudera.cdp.airflow.operators.cdw_operator import CDWOperator
+    
 from cloudera.cdp.airflow.operators.cde_operator import CDEJobRunOperator
 
 default_args = {
@@ -24,3 +30,51 @@ dag = DAG(
     is_paused_upon_creation=False
     )
 
+cdw_query = """Create external table external_table(name string,age int,address string,zip int)row format delimitedfields terminated by ','stored as textfilelocation '/test/abc';"""
+
+Create_External_Table_Step = CDWOperator(
+    task_id="Create_External_Table",
+    dag=dag,
+    cli_conn_id="hive_conn",
+    hql=cdw_query,
+    schema='default',
+    ### CDW related args ###
+    use_proxy_user=False,
+    query_isolation=True
+)
+
+cdw_query = """Create Table orc_table(name string, -- Concate value of first name and last name with space as seperatoryearofbirth int,age int, -- Current year minus year of birthaddress string,zip int)STORED AS ORC;"""
+
+Create_orc_Table_Step = CDWOperator(
+    task_id="Create_orc_Table",
+    dag=dag,
+    cli_conn_id="hive_conn",
+    hql=cdw_query,
+    schema='default',
+    ### CDW related args ###
+    use_proxy_user=False,
+    query_isolation=True
+)
+
+cdw_query = """use "default"; -- input from Oozieinsert into table orc_tableselectconcat(first_name,' ',last_name) as name,yearofbirth,year(from_unixtime) --yearofbirth as age,address,zipfrom external_table;"""
+
+Insert_into_Table_Step = CDWOperator(
+    task_id="Insert_into_Table",
+    dag=dag,
+    cli_conn_id="hive_conn",
+    hql=cdw_query,
+    schema='default',
+    ### CDW related args ###
+    use_proxy_user=False,
+    query_isolation=True
+)
+
+
+def my_func():
+	 pass
+ 
+'StepStub' = PythonOperator(task_id='PythonOperator', python_callable=my_func)
+def my_func():
+	pass
+ 
+StepStub = PythonOperator(task_id='PythonOperator', python_callable=my_func)
